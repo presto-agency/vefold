2ec5de4d116e142f97dc0c9ce339af927283a2b1
artemats <artem.matseruk@gmail.com>
2020-11-13 15:22:56 +0200

    setup svg sprite for IE11

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,10 @@ const check = document.getElementById('check');
 if(btn) {
     btn.addEventListener('click', function (event) {
         event.preventDefault();
-        console.log();
         if(check.checked) {
             window.location.href = '/signup-notification.html';
         } else {
-            btn.previousSibling.previousElementSibling.classList.add('error');
+            check.parentElement.parentElement.classList.add('error');
         }
     });
 }
